af6b10b5a9f62657ad5dc5a73d1496a1f51a81f9
jjeong <109637866+leejjeonghui@users.noreply.github.com>
2024-01-17 15:02:50 +0900

    [level 0] Title: qr code, Time: 0.07 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,12 @@
+class Solution {
+    public StringBuilder solution(int q, int r, String code) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<code.length();i++){
+            if(i%q==r){
+                sb.append(code.charAt(i));
+            }
+        }
+        return sb;
+    }
+}
