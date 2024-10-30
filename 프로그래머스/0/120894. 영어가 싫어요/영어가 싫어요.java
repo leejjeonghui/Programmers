7d5be4cf720ce8bff08c5b3a16ce56b94d79bcd8
jeonghui <109637866+leejjeonghui@users.noreply.github.com>
2024-10-30 21:18:46 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,20 +1,11 @@
-import java.util.*;
-
 class Solution {
     public long solution(String numbers) {
-        long answer = 0;
         String[] numArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        HashMap<String,Integer> map = new HashMap<>();
 
         for (int i = 0; i < numArr.length; i++) {
-            map.put(numArr[i],i);
+            numbers = numbers.replace(numArr[i], String.valueOf(i));
         }
 
-        for (int j = 0; j < numArr.length; j++) {
-            if(numbers.contains(numArr[j])){
-              numbers=numbers.replaceAll(numArr[j],String.valueOf(map.get(numArr[j])));
-            }
-        }       
         return Long.parseLong(numbers);
     }
 }
