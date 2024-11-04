cf0b33b12fe3b47ce244c31e66909b673f94dc33
jeonghui <109637866+leejjeonghui@users.noreply.github.com>
2024-11-04 20:50:33 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.09 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        long temp = (long)Math.sqrt(n);
+        
+        if(temp*temp ==n){
+            answer = (temp+1)*(temp+1);
+        }else{
+            return -1;
+        }
+        return answer;
+    }
+}
